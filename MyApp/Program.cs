87e064ef5c769d9d374bd3ce37528df6dacdc516
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2022-12-20 03:27:45 -0500

    learn the C# basics

---
+++ MyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    namespace MyApp
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter your city: ");
+                string cityName = Console.ReadLine();
+                Console.Write("Please enter the zipcode: ");
+                string myZipCode = Console.ReadLine();
+                Console.WriteLine("Your city is " + cityName + " and your zip code is " + myZipCode + ".");
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+
+

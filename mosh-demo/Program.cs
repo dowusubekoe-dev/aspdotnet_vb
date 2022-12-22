70e4a201799fb84d3cee47b986f45038ba571208
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2022-12-22 01:22:44 -0500

    updated mosh-demo folder

---
+++ mosh-demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = ++a;
+
+            System.Console.WriteLine("{0}", b);
+            System.Console.WriteLine("{0}", a);
+
+
+
+        Console.ReadKey();
+        }
+    }
+}
